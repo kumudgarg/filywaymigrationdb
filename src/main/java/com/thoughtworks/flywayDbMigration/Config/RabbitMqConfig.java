78b32748294e203928fee1531fba7424f7933839
kumud garg <kumud.garg@kumuds-MacBook-Pro.local>
2020-04-10 04:38:21 +0530

    Resolved: issue connection failure between mysql and user_info app container and make new container for flyway migration

--- src/main/java/com/thoughtworks/flywayDbMigration/Config/RabbitMqConfig.java
+++ src/main/java/com/thoughtworks/flywayDbMigration/Config/RabbitMqConfig.java
@@ -1,63 +1,63 @@
-//package com.thoughtworks.flywayDbMigration.Config;
+package com.thoughtworks.flywayDbMigration.Config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:listener-rabbitmq.properties")
+public class RabbitMqConfig {
+
+//    @Value("${users.rabbitmq.queue}")
+//    private String queueName;
 //
-//import org.springframework.amqp.core.*;
-//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
-//import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-//import org.springframework.amqp.support.converter.MessageConverter;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
+//    @Value("${users.rabbitmq.exchange}")
+//    private String exchange;
 //
-//@Configuration
-//@PropertySource("classpath:listener-rabbitmq.properties")
-//public class RabbitMqConfig {
-//
-////    @Value("${users.rabbitmq.queue}")
-////    private String queueName;
-////
-////    @Value("${users.rabbitmq.exchange}")
-////    private String exchange;
-////
-////    @Value("${users.rabbitmq.routingkey}")
-////    private String routingKey;
-//
-//    @Bean
-//    Queue queue(){
-//        return new Queue("abcd.queue", false);
-//    }
-//
-//    @Bean
-//    DirectExchange exchange(){
-//        return new DirectExchange("direct");
-//    }
-//
-//    @Bean
-//    public Binding binding(Queue queue, DirectExchange directExchange){
-//        return BindingBuilder.bind(queue).to(directExchange).with("routingKey");
-//    }
-//
-//    @Bean
-//    public MessageConverter jsonMessageConverter(){
-//        return new Jackson2JsonMessageConverter();
-//    }
-//
-//    @Bean
-//    public AmqpTemplate rabbiTemplate(ConnectionFactory connectionFactory){
-//        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-//        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-//        return rabbitTemplate;
-//    }
-//    @Bean
-//    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory){
-//        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
-//        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
-//        simpleMessageListenerContainer.setQueues(queue());
-//        return simpleMessageListenerContainer;
-//    }
-//
-//
-//}
+//    @Value("${users.rabbitmq.routingkey}")
+//    private String routingKey;
+
+    @Bean
+    Queue queue(){
+        return new Queue("abcd.queue", false);
+    }
+
+    @Bean
+    DirectExchange exchange(){
+        return new DirectExchange("direct");
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange directExchange){
+        return BindingBuilder.bind(queue).to(directExchange).with("routingKey");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate rabbiTemplate(ConnectionFactory connectionFactory){
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+    @Bean
+    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory){
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue());
+        return simpleMessageListenerContainer;
+    }
+
+
+}
